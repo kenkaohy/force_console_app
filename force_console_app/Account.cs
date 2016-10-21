2cd20a599a37bf0f23232867b60eca1851371645
kenkaohy <ken.kao.hy@gmail.com>
2016-10-21 14:54:28 -0700

    initial

---
+++ force_console_app/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace force_console_app
+{
+    //class map to SFDC Account Object.
+    public class Account
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+         
+}
--- force_console_app/Program.cs
+++ force_console_app/Program.cs
@@ -3,13 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Salesforce.Common;
+using Salesforce.Force;
 
 namespace force_console_app
 {
     class Program
     {
+        private static string clientId = "3MVG9ZL0ppGP5UrCIsahMReg5jlcgS0fKX6yJEYrfgaPiVhnW5fIroX.CgFmYD1SFmQMBIM8n7CKAJH2dvgm2";
+        private static string clientSecret = "3228504275119711645";
+        private static string username = "";//SFDC account
+        private static string password = "";//SFDC password + token
+        
         static void Main(string[] args)
         {
+            //initial SFDC authentication.
+            var auth = new AuthenticationClient();
+            auth.UsernamePasswordAsync(clientId, clientSecret, username, password).Wait();
+
+            //establish Force.com transaction.
+            var client = new ForceClient(auth.InstanceUrl, auth.AccessToken, auth.ApiVersion);
+
+            //SFDC SOQL string.
+            var strQuery = "SELECT Id, Name FROM Account LIMIT 1000";
+
+            //Define the return object type and result.
+            var result = client.QueryAsync<Account>(strQuery);
+            result.Wait();
+
+            var accounts = result.Result.Records;
+
+            foreach(var acc in accounts)
+            {
+                Console.WriteLine(acc.Name);
+                
+            }
+
+
+
+
         }
     }
 }
