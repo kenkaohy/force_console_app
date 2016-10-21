72578c9c583dfc1b410b89355a2a2e505c56943c
kenkaohy <ken.kao.hy@gmail.com>
2016-10-21 13:29:53 -0700

    Add project files.

---
+++ force_console_app/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace force_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
